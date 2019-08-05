2a9d6668bea85853f05cf77c5634152822d82e58
afreen shaik <sk.afreen12@gmail.com>
2019-08-05 09:40:33 +0530

    application properties are added

--- src/main/java/com/stackroute/seeddata/CommandLineRunnerImpl.java
+++ src/main/java/com/stackroute/seeddata/CommandLineRunnerImpl.java
@@ -2,20 +2,39 @@ package com.stackroute.seeddata;
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+// It will get execute, after the server get starts
+
 @Component
+//Property source annotation is used to externalize configuration to a property file.
+@PropertySource("classpath:application.properties")
 public class CommandLineRunnerImpl implements CommandLineRunner {
+    //    Value annotation is used for injecting values into fields in Spring-managed beans
+    @Value("${track.id}")
+    private int id;
+    @Value("${track.name}")
+    private String name;
+    @Value("${track.comment}")
+    private String comment;
+    Track track = new Track();
     private TrackRepository trackRepository;
+
+    //    constructor
     @Autowired
     public CommandLineRunnerImpl(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
     }
+
     @Override
     public void run(String... args) throws Exception {
-        Track track1=new Track(1,"afreen","good");
-        trackRepository.save(track1);
-        Track track2=new Track(3,"shaik","awesome");
-        trackRepository.save(track2);
+        track.setId(id);
+        track.setName(name);
+        track.setComment(comment);
+        trackRepository.save(track);
     }
+
 }
